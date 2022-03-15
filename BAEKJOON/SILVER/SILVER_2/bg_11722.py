2e726b4ab7b5fda989390c2a1cdc9216f50e8315
tmdgh1592 <tmdgh1592@naver.com>
2022-03-16 04:29:41 +0900

    백준 11722번 문제 풀이

--- BAEKJOON/SILVER/SILVER_2/bg_11722.py
+++ BAEKJOON/SILVER/SILVER_2/bg_11722.py
@@ -1,7 +1,15 @@
 from bisect import bisect_left
 
+# {10 30 10 20 20 10} =>
+# {-10 -30 -10 -20 -20 -10} =>
+# {-30 -20 -10} =>
+# {30 20 10}
+
+def minus(x):
+    return -1 * int(x)
+
 n = int(input())
-arr = list(map(int, input().split()))
+arr = list(map(minus, input().split()))
 dp = [arr[0]]
 
 for i in range(1, n):
