b8478bf47e996f4ac509be831e68224ef27bc8a9
tmdgh1592 <tmdgh1592@naver.com>
2022-03-25 20:02:21 +0900

    백준 2468번 문제 풀이

--- BAEKJOON/SILVER/SILVER_1/bg_2468.py
+++ BAEKJOON/SILVER/SILVER_1/bg_2468.py
@@ -1,46 +1,51 @@
 import copy
 import sys
-sys.setrecursionlimit(10 ** 8)
+sys.setrecursionlimit(10 ** 6)
+visited = -1
+
+
+def dfs(graph, x, y, nh):
+    if x < 0 or y < 0 or x >= n or y >= n:
+        return False
+
+    if graph[x][y] >= nh:
+        # 방문처리
+        graph[x][y] = visited
+
+        for i in range(4):
+            dfs(graph, x+dx[i], y+dy[i], nh)
+
+        return True
+    return False
 
 
 n = int(input())
-org_graph = []
+org_graph = []  # N x N 기둥 그래프
+hh = -1  # 기둥의 최대 높이
 
 for _ in range(n):
-    org_graph.append(list(map(int, input().split())))
+    h_arr = list(map(int, input().split()))
+    hh = max([hh]+h_arr)  # 기둥 최대 높이 갱신
+    org_graph.append(h_arr)
 
 # 상하좌우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def dfs(graph, x, y, now_height):
-    if x < 0 or y < 0 or x >= n or y >= n:
-        return False
-
-    if graph[x][y] < now_height:
-        return False
-
-    # 방문처리
-    graph[x][y] = 0
-
-    for i in range(4):
-        dfs(graph, x+dx[i], y+dy[i], now_height)
-
-    return True
 
 max_result = 0
 max_height = 0
-for h in range(10):
+
+for h in range(hh+1):
     result = 0
+    graph = copy.deepcopy(org_graph)
     for i in range(n):
         for j in range(n):
-            if dfs(copy.deepcopy(org_graph), i, j, h):
+            if dfs(graph, i, j, h):
                 result += 1
 
     if max_result < result:
         max_result = result
         max_height = h
 
-    
-
-print(max_height)
+print(max_result)
