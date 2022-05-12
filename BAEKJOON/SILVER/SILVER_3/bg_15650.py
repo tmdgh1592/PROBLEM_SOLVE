25200b3445c74f9e75dc07d2aee350204ad99248
tmdgh1592 <tmdgh1592@naver.com>
2022-05-12 22:48:39 +0900

    백준 15650번 문제 풀이

---
+++ BAEKJOON/SILVER/SILVER_3/bg_15650.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+result = [0]
+
+
+def dfs(n, m):
+
+    if len(result) == m+1:
+        print(*result[1:])
+        return
+
+    for i in range(1, n+1):
+        if i not in result and i > result[-1]:
+            result.append(i)
+            dfs(n, m)
+            result.pop()
+
+
+dfs(n, m)
