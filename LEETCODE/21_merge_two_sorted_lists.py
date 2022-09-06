d19ce2571c45ad2653830df7fb5c8a10ef79af3b
tmdgh1592 <tmdgh1592@naver.com>
2022-09-06 11:50:30 +0900

    리트코드 21번 문제 풀이

---
+++ LEETCODE/21_merge_two_sorted_lists.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+from typing import Optional
+
+
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        my_list = []
+
+        while l1 or l2:
+            if l1:
+                my_list.append(l1.val)
+                l1 = l1.next
+            elif l2:
+                my_list.append(l2.val)
+                l2 = l2.next
+
+        my_list.sort()
+
+        mergedList = head = None
+
+        for val in my_list:
+            if not mergedList:
+                mergedList = ListNode(val)
+                head = mergedList
+            else:
+                mergedList.next = ListNode(val)
+                mergedList = mergedList.next
+
+        return head
+            
+            
+
