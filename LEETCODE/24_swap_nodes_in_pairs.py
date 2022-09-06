733d3d46a0df84051854b791ba11bb5f89ce9b52
tmdgh1592 <tmdgh1592@naver.com>
2022-09-06 16:53:02 +0900

    리트코드 24번 문제풀이

---
+++ LEETCODE/24_swap_nodes_in_pairs.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+
+        while cur and cur.next:
+            cur.val, cur.next.val = cur.next.val, cur.val
+            cur = cur.next.next
+
+        return head
