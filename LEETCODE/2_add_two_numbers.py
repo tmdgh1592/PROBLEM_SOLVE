148154630dfa09bdba11b2cc34afbe13c322d8e9
tmdgh1592 <tmdgh1592@naver.com>
2022-09-06 16:32:58 +0900

    리트코드 2번 전가산기로 문제풀이

--- LEETCODE/2_add_two_numbers.py
+++ LEETCODE/2_add_two_numbers.py
@@ -1,6 +1,6 @@
 # Definition for singly-linked list.
-
 from typing import Optional
+
 
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -8,39 +8,24 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        node, prev = head, None
-
-        while node:
-            next, node.next = node.next, prev
-            prev, node = node, next
-
-        return prev
-
-    def toList(self, node: ListNode) -> list:
-        my_list = []
-
-        while node:
-            my_list.append(node.val)
-            node = node.next
-
-        return my_list
-            
-    def toReversedLinkedList(self, result:str) -> ListNode:
-        prev: ListNode = None
-
-        for r in result:
-            node = ListNode(r)
-            node.next = prev
-            prev = node
-        
-        return node
-
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        reversedList1 = self.toList(self.reverseList(l1))
-        reversedList2 = self.toList(self.reverseList(l2))
+        root = head = ListNode(0)
+        carry = 0
+    
 
-        num1 = ''.join(str(e) for e in reversedList1)
-        num2 = ''.join(str(e) for e in reversedList2)
+        while l1 or l2 or carry:
+            sum = 0
+            
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
 
-        return self.toReversedLinkedList(str(int(num1) + int(num2)))
+            carry, val = divmod(sum+carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+        
+
+        return root.next
