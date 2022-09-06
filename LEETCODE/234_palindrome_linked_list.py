9ec846d362ffa0e156f1e9f06c008fb5d0cf818c
tmdgh1592 <tmdgh1592@naver.com>
2022-09-06 11:13:10 +0900

    리트코드 234번 Runner로 리팩토링 풀이

--- LEETCODE/234_palindrome_linked_list.py
+++ LEETCODE/234_palindrome_linked_list.py
@@ -7,23 +7,43 @@ class ListNode:
 from collections import deque
 from typing import Optional
 
-# 리스트로 변환하여 풀ㅣ
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if not head:
-            return True
+        rev = None
+        slow = fast = head
 
-        q = deque()
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
 
-        node = head
+        # fast가 None이 아니면 홀수개의 입력임.
+        if fast:
+            slow = slow.next #중앙값은 팰린드롬 고려에서 제외하기 위해 한 칸 더 이동
 
-        while node is not None:
-            q.append(node.val)
-            node = node.next
+        while rev and rev.val == slow.val:
+            rev, slow = rev.next, slow.next
+        
+        return not rev
+
+
+
+# 리스트로 변환하여 풀이
+# class Solution:
+#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+#         if not head:
+#             return True
+
+#         q = deque()
+
+#         node = head
+
+#         while node is not None:
+#             q.append(node.val)
+#             node = node.next
 
         
-        while len(q) > 1:
-            if q.popleft() != q.pop():
-                return False
+#         while len(q) > 1:
+#             if q.popleft() != q.pop():
+#                 return False
         
-        return True
+#         return True
