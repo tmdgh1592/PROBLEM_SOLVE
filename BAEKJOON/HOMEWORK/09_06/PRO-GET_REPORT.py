134ee2aa028c85bc66ee6b465180c5017ab48935
tmdgh1592 <tmdgh1592@naver.com>
2022-09-12 22:57:31 +0900

    프로그래머스 get report 문제풀이

--- BAEKJOON/HOMEWORK/09_06/PRO-GET_REPORT.py
+++ BAEKJOON/HOMEWORK/09_06/PRO-GET_REPORT.py
@@ -1,6 +1,18 @@
-import sys
+from collections import defaultdict
 
-sys.stdin = open('input.txt', 'r')
-input = sys.stdin.readline
-MIS = lambda: map(int, input().rstrip().split())
 
+def solution(id_list, report, k):
+    answer = {id:0 for id in id_list}
+    reported = {id:set() for id in id_list}
+
+
+    for r in report:
+        user, reported_user= r.split()
+        reported[reported_user].add(user)
+
+    for reported_user, users in reported.items():
+        if len(users) >= k:
+            for user in users:
+                answer[user] += 1
+
+    return list(answer.values())
