61b22f46489434d1db3f074f99c00f9d4c983227
tmdgh1592 <tmdgh1592@naver.com>
2022-09-18 20:38:06 +0900

    백준 2607번 문제풀이

--- BAEKJOON/HOMEWORK/09_20/BOJ_2607.py
+++ BAEKJOON/HOMEWORK/09_20/BOJ_2607.py
@@ -1,6 +1,36 @@
 #-*- coding:utf-8 -*-
+from collections import Counter
 import sys
 
-sys.stdin = open('input.txt', 'r')
 input = sys.stdin.readline
 MIS = lambda: map(int, input().rstrip().split())
+
+n = int(input())
+first = input().rstrip()
+words = [input().rstrip() for _ in range(n-1)]
+
+total = 0
+aLen = len(first)
+
+for word in words:
+    bLen = len(word)
+    if abs(aLen - bLen) >= 2:
+        continue
+
+    aChecked = [False] * aLen
+    bChecked = [False] * bLen
+
+    for i, aCh in enumerate(first):
+        for j, bCh in enumerate(word):
+            if not bChecked[j] and aCh == bCh:
+                aChecked[i] = True
+                bChecked[j] = True
+                break
+
+    aCount = aChecked.count(False)
+    bCount = bChecked.count(False)
+
+    if aCount <= 1 and bCount <= 1 and abs(aCount - bCount) <= 1:
+        total += 1
+
+print(total)
