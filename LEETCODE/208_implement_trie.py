c6cfff4ce33ce8065a7e8ab90bf1e03886d7ba12
tmdgh1592 <tmdgh1592@naver.com>
2022-09-22 00:23:29 +0900

    리트코드 208번 문제풀이

---
+++ LEETCODE/208_implement_trie.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+
+
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = defaultdict(TrieNode)
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for ch in word:
+            cur = cur.children[ch]
+        cur.word = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for ch in word:
+            if ch not in cur.children:
+                return False
+            cur = cur.children[ch]
+
+        return cur.word
+        
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for ch in prefix:
+            if ch not in cur.children:
+                return False
+            cur = cur.children[ch]
+
+        return True
