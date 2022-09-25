9bba370314386d682eee653a7036532ae8c69673
tmdgh1592 <tmdgh1592@naver.com>
2022-09-25 12:03:02 +0900

    백준 10989번 문제풀이

--- BAEKJOON/CLASS/CLASS2/BOJ_10989.py
+++ BAEKJOON/CLASS/CLASS2/BOJ_10989.py
@@ -1,9 +1,18 @@
 #-*- coding:utf-8 -*-
+from collections import defaultdict
 import sys
 
 sys.stdin = open('input.txt', 'r')
 input = sys.stdin.readline
 MIS = lambda: map(int, input().rstrip().split())
+
 n = int(input())
-arr = [int(input()) for _ in range(n)]
-print(*sorted(arr), sep='\n')
+arr = [0] * (10001)
+
+# 계수정렬 O(N+K)
+for _ in range(n):
+    arr[int(input())] += 1
+
+for i in range(10001):
+    for _ in range(arr[i]):
+        print(i, end='\n')
