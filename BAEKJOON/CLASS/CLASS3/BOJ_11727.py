4ad3e9da9baf227db222528c66103ab7120556f6
tmdgh1592 <tmdgh1592@naver.com>
2022-09-27 16:49:30 +0900

    백준 11727번 문제풀이

---
+++ BAEKJOON/CLASS/CLASS3/BOJ_11727.py
@@ -0,0 +1,17 @@
+#-*- coding:utf-8 -*-
+import sys
+
+input = sys.stdin.readline
+MIS = lambda: map(int, input().rstrip().split())
+
+n = int(input())
+dp = [0] * (1001)
+dp[1], dp[2] = 1, 3
+
+def f(i):
+    if dp[i] != 0:
+        return dp[i] % 10007
+    dp[i] = (f(i-1) + 2 * f(i-2)) % 10007
+    return dp[i]
+
+print(f(n))
