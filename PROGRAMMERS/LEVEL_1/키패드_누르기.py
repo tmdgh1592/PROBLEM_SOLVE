3b1358957ce85c1818b59166dd39fa6f06c2c581
tmdgh1592 <tmdgh1592@naver.com>
2022-10-04 16:29:12 +0900

    [Programmers Level 1] 키패드 누르기 문제풀이 (재풀이)

--- PROGRAMMERS/LEVEL_1/키패드_누르기.py
+++ PROGRAMMERS/LEVEL_1/키패드_누르기.py
@@ -1,33 +1,46 @@
 def solution(numbers, hand):
     answer = ''
-    left_num = 10
-    right_num = 11
-    num_xy = [(1, 0), (0, 3), (1, 3), (2, 3), (0, 2),  # 0 1 2 3 4
-              (1, 2), (2, 2), (0, 1), (1, 1), (2, 1),  # 5 6 7 8 9
-              (0, 0), (2, 0)]                            # 10 11
+    left = '*'
+    right = '#'
+    distance = {
+        '*' : [4, 3, 2, 1],
+        '#' : [4, 3, 2, 1],
+        0 : [3, 2, 1, 0],
+        1 : [1, 2, 3, 4],
+        2 : [0, 1, 2, 3],
+        3 : [1, 2, 3, 4],
+        4 : [2, 1, 2, 3],
+        5 : [1, 0, 1, 2],
+        6 : [2, 1, 2, 3],
+        7 : [3, 2, 1, 2],
+        8 : [2, 1, 0 ,1],
+        9 : [3, 2, 1, 2]
+    }
+    converter = {2 : 0, 5 : 1, 8 : 2, 0 : 3}
 
-    for num in numbers:
-
-        if num in [1, 4, 7]:
+    for number in numbers:
+        if number in [1, 4, 7]:
             answer += 'L'
-            left_num = num
-        elif num in [3, 6, 9]:
+            left = number
+        elif number in [3, 6, 9]:
             answer += 'R'
-            right_num = num
-        else:
-            press_num = num_xy[num]
-
-            left_dis = (abs(press_num[0] - num_xy[left_num][0]))\
-                + abs(press_num[1] - num_xy[left_num][1])
-
-            right_dis = abs((press_num[0] - num_xy[right_num][0]))\
-                + abs(press_num[1] - num_xy[right_num][1])
-
-            if left_dis > right_dis or (left_dis == right_dis and hand == 'right'):
-                answer += 'R'
-                right_num = num
-            elif left_dis < right_dis or (left_dis == right_dis and hand == 'left'):
+            right = number
+        else:        
+            left_dist = distance[left][converter[number]]
+            right_dist = distance[right][converter[number]]
+            
+            if left_dist < right_dist:
                 answer += 'L'
-                left_num = num
+                left = number
+            elif left_dist > right_dist:
+                answer += 'R'
+                right = number
+            else:
+                if hand == 'right':
+                    answer += 'R'
+                    right = number
+                else:
+                    answer += 'L'
+                    left = number
 
     return answer
