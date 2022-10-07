d5c679e9a69c8491bf9d328c3b38639f3c2392e8
tmdgh1592 <tmdgh1592@naver.com>
2022-10-07 10:13:45 +0900

    백준 16236번 문제풀이

--- BAEKJOON/HOMEWORK/09_26/BOJ_16236.py
+++ BAEKJOON/HOMEWORK/09_26/BOJ_16236.py
@@ -1,35 +1,50 @@
 #-*- coding:utf-8 -*-
-from collections import deque
+import heapq
 import sys
 
-sys.stdin = open('input.txt', 'r')
 input = sys.stdin.readline
 MIS = lambda: map(int, input().rstrip().split())
 
 n = int(input())
-graph = []
+graph = [list(MIS()) for _ in range(n)]
 visited = [[False] * n for _ in range(n)]
-opers = [(-1, 0), (0, -1), (1, 0) , (0, 1)]
-sx = sy = 0
+q = []
 
-flag = True
 for i in range(n):
-    row = list(MIS())
-    graph.append(row)
     for j in range(n):
-        # 먹을 수 있는게 하나라도 있다면
-        if row[j] == 1: flag = False
-        if row[j] == 9:
-            sx = i; sy = j
+        if graph[i][j] == 9:
+            graph[i][j] = 0
+            visited[i][j] = True
+            q.append((0, i, j))
 
-# 먹을게 하나도 없으면 0 출력 후 종료
-if flag:
-    print(0)
-    sys.exit()
+body = 2
+ate = res = 0
 
-q = deque([(sx, sy, 0)])
+opers = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
 while q:
-    x, y, time = q.popleft()
+    distance, x, y = heapq.heappop(q)
 
+    if 0 < graph[x][y] < body:
+        ate += 1
+        res += distance
+        graph[x][y] = 0
 
+        if ate == body:
+            body += 1
+            ate = 0
 
+        q = []
+        distance = 0
+        visited = [[False] * n for _ in range(n)]
+        visited[x][y] = True
+    
+    for oper in opers:
+        nx, ny = x+oper[0], y+oper[1]
+        if not (0 <= nx < n and 0 <= ny < n): continue
+        if visited[nx][ny]: continue
+        if graph[nx][ny] > body: continue
+        visited[nx][ny] = True
+        heapq.heappush(q, (distance + 1, nx, ny))
+
+print(res)
