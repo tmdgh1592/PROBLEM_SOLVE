d7a9a9a842ade951c97a9fc3d9fcc30871067b38
tmdgh1592 <tmdgh1592@naver.com>
2022-11-24 19:42:39 +0900

    백준 11404번 문제풀이

---
+++ BAEKJOON/HOMEWORK/09_20/BOJ_11404.py
@@ -0,0 +1,30 @@
+#-*- coding:utf-8 -*-
+import sys
+
+sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
+MIS = lambda: map(int, input().rstrip().split())
+INF = float('inf')
+
+n, m = [int(input()) for _ in range(2)]
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    graph[i][i] = 0
+
+for _ in range(m):
+    a, b, c = MIS()
+    graph[a][b] = min(graph[a][b], c)
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if graph[i][j] == INF:
+            print(0, end = ' ')
+        else:
+            print(graph[i][j], end = ' ')
+    print()
