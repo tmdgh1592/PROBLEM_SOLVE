93678d9bae2c5bbd76c1f605fe82676e6244fc6a
tmdgh1592 <tmdgh1592@naver.com>
2022-11-29 00:44:53 +0900

    fix(index error): 백준 9465번 문제풀이

--- BAEKJOON/CLASS/CLASS4/BOJ_9465.py
+++ BAEKJOON/CLASS/CLASS4/BOJ_9465.py
@@ -6,8 +6,10 @@ MIS = lambda: map(int, input().rstrip().split())
 for _ in range(int(input())):
     n = int(input())
     data = [list(MIS()) for _ in range(2)]
-    data[0][1] += data[1][0]
-    data[1][1] += data[0][0]
+    
+    if n >= 2:
+        data[0][1] += data[1][0]
+        data[1][1] += data[0][0]
     
     for i in range(2, n):
             data[0][i] += max(data[1][i-1], data[1][i-2], data[0][i-2])
