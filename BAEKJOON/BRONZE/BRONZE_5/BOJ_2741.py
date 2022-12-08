f1de0f9b26c78669056d0290c7d6ee2fedc71a7b
tmdgh1592 <tmdgh1592@naver.com>
2022-12-08 10:23:33 +0000

    백준 2741번 문제풀이

---
+++ BAEKJOON/BRONZE/BRONZE_5/BOJ_2741.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+import sys
+
+sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
+MIS = lambda: map(int, input().rstrip().split())
+
+for i in range(1, int(input()) + 1): print(i)
