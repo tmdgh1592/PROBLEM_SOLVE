eb564d7e3a34f02eeb7906f0135f89b7dd790588
tmdgh1592 <tmdgh1592@naver.com>
2023-06-19 02:20:57 +0900

    백준 19638번 문제풀이

---
+++ BAEKJOON/HOMEWORK/23_06_16/BOJ_19638.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+#define FAST_IO \
+    if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
+#define debug(x) \
+    if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugf(...) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "; printf(__VA_ARGS__); }
+#define debugc(c) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int res, n, h, t, e;
+priority_queue<int> q;
+
+int main(){
+    FAST_IO;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cin >> n >> h >> t;
+    rep(i, 0, n) {
+        cin >> e;
+        q.push(e);
+    }
+
+    while(t--) {
+        if (h > q.top()) break;
+        if (q.top() != 1) {
+            int half = q.top() / 2;
+            q.pop();
+            q.push(half);
+        }
+        res++;
+    }
+
+    if (h > q.top()) {
+        cout << "YES" << endl << res;
+    } else {
+        cout << "NO" << endl << q.top();
+    }
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
