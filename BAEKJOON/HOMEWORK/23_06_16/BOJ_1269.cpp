f2a5c30909b725be6c7976544aabc781f7ec151c
tmdgh1592 <tmdgh1592@naver.com>
2023-06-19 00:55:22 +0900

    백준 1269번 문제풀이

---
+++ BAEKJOON/HOMEWORK/23_06_16/BOJ_1269.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+#define FAST_IO \
+    if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
+#define debug(x) \
+    if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugf(...) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "; printf(__VA_ARGS__); }
+#define debugc(c) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int n, m, num;
+set<int> s1, s2;
+
+set<int> difference(set<int> set1, set<int> set2) {
+    set<int> res;
+
+    for (auto e : set1) {
+        if (set2.find(e) == set2.end()) res.insert(e);
+    }
+    return res;
+}
+
+int main(){
+    FAST_IO;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cin >> n >> m;
+    rep(i, 0, n) {
+        cin >> num;
+        s1.insert(num);
+    }
+    rep(i, 0, m) {
+        cin >> num; 
+        s2.insert(num);
+    }
+
+
+    cout << difference(s1, s2).size() + difference(s2, s1).size();
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
