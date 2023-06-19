e33a7a76e458b43ec1910f8c193a8941624c10a3
tmdgh1592 <tmdgh1592@naver.com>
2023-06-20 00:32:39 +0900

    백준 17484번 문제풀이

---
+++ BAEKJOON/HOMEWORK/23_06_16/BOJ_17484.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+#define FAST_IO \
+    if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
+#define debug(x) \
+    if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugf(...) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "; printf(__VA_ARGS__); }
+#define debugc(c) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int n, m, ans = INF;
+int board[7][7];
+int dirs[3] = {-1, 0, 1};
+
+int f(int r, int c, int dir) {
+    if (r == n) return 0;
+
+    int res = INF;
+    rep(i, 0, 3) {
+        if (dir == i) continue;
+        if (!(0 <= c + dirs[i] && c + dirs[i] < m)) continue;
+        res = min(res, board[r][c] + f(r + 1, c + dirs[i], i));
+    }
+
+    return res;
+}
+
+int main(){
+    FAST_IO;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cin >> n >> m;
+    rep(i, 0, n) rep(j, 0, m) cin >> board[i][j];
+    rep(i, 0, m) ans = min(ans, f(0, i, -1));
+
+    cout << ans << endl;
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
