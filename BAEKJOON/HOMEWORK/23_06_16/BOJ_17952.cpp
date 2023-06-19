421bd174573ce4f641dc20409f8704fb59199825
tmdgh1592 <tmdgh1592@naver.com>
2023-06-19 20:49:44 +0900

    백준 17952번 문제풀이

---
+++ BAEKJOON/HOMEWORK/23_06_16/BOJ_17952.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+#define FAST_IO \
+    if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
+#define debug(x) \
+    if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugf(...) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "; printf(__VA_ARGS__); }
+#define debugc(c) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int n, res;
+int a, score, t;
+stack<pii> s; // score, time
+
+int main(){
+    FAST_IO;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cin >> n;
+    rep(i, 0, n) {
+        cin >> a;
+        
+        if (a) {
+            cin >> score >> t;
+            if ((t - 1) == 0) res += score;
+            else s.push({score, t - 1});
+        } else {
+            if (!s.empty()) {
+                pii top = s.top();
+                int remain = top.second - 1;
+                if (!remain) { 
+                    s.pop();
+                    res += top.first;
+                } else {
+                    s.pop();
+                    s.push({top.first, remain});
+                }
+            }
+        }
+    }
+    
+    cout << res;
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
