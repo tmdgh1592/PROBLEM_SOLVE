b62e5015516f2586d84720fffe2a7078f667ee0d
tmdgh1592 <tmdgh1592@naver.com>
2023-06-24 15:59:34 +0900

    백준 2531번 문제풀이

---
+++ BAEKJOON/HOMEWORK/23_06_16/BOJ_2531.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+#define FAST_IO \
+    if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
+#define debug(x) \
+    if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugf(...) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "; printf(__VA_ARGS__); }
+#define debugc(c) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int n, d, k, c, ans;
+vector<int> vec;
+
+int main(){
+    FAST_IO;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cin >> n >> d >> k >> c;
+    vec.resize(n);
+    for (auto& e : vec) cin >> e;
+    rep(i, 0, k - 1) vec.pb(vec[i]);
+    
+    for(int i = 0; i < n; i++) {
+        bool select[d + 1];
+        int count = 0;
+        for (int j = i; j < i + k; j++) {
+            if (!select[vec[j]]) {
+                count++;
+                select[vec[j]] = true;
+            }
+        }
+        ans = max(ans, count + !select[c]);
+        memset(select, false, sizeof(select));
+    }
+
+    cout << ans << endl;
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
