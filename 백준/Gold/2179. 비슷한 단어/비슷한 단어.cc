451c8b77f59a9dac5092c80bbac6ec9af5bf6667
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2023-06-27 00:02:26 +0900

    [Gold IV] Title: 비슷한 단어, Time: 720 ms, Memory: 2912 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.cc
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.cc
@@ -1,81 +1,72 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
 using namespace std;
-int min(int a,int b){
-    if(a<b){
-        return a;
-    }
-    else{
-        return b;
-    }
-}
 
-int max(int a,int b){
-    if(a>b){
-        return a;
-    }
-    else{
-return b;}
-}
-int main(void){
-    string arr[20001];
-    int n;
-    int max_size = 0;
-    int l = n;
-    int r = n;
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+#define FAST_IO \
+    if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
+#define debug(x) \
+    if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugf(...) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "; printf(__VA_ARGS__); }
+#define debugc(c) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int n;
+int q, r, max_len;
+string e;
+string arr[20000];
+
+int main(){
+    FAST_IO;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
 
     cin >> n;
-    for(int i = 0;i<n;i++){
-        cin >> arr[i];
+    rep(i, 0, n) cin >> arr[i];
 
-    }
-
-    for(int i = 0;i<n;i++){
-        for(int j = i+1;j<n;j++){
-            if(arr[i] == arr[j]){
-                continue;
+    rep(i, 0, n - 1) {
+        rep(j, i + 1, n) {
+            if (arr[i] == arr[j]) continue;
+            int count = 0, len = min(arr[i].size(), arr[j].size());
+            
+            rep(k, 0, len) {
+                if (arr[i][k] != arr[j][k]) break;
+                count++;
             }
-            int length = min(arr[i].size(),arr[j].size());
-            if(length == arr[i].size()){
-                int temp = 0;
-                for(int k = 0;k<arr[i].size();k++){
-                    if(arr[i][k] != arr[j][k]){
-                        break;
-                    }
-                    temp++;
-                }
-
-                if(temp > max_size){
-                    max_size = temp;
-                    l = i;
-                    r = j;
-                }
-
+            
+            if (max_len < count) {
+                max_len = count;
+                q = i;
+                r = j;
             }
-           else if(length == arr[j].size()){
-               int temp = 0;
-               for(int k = 0;k<arr[j].size();k++){
-                   if(arr[i][k] != arr[j][k]){
-                       break;
-                   }
-                   temp++;
-
-               }
-
-               if(temp > max_size){
-                   max_size = temp;
-                   l = i;
-                   r = j;
-               }
-
-           }                  
-
         }
     }
-     int small = min(l,r);
-     int big = max(l,r);
 
-    cout << arr[small] << '\n';
-     cout << arr[big];
+    cout << arr[q] << endl;
+    cout << arr[r] << endl;
 
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
 }
