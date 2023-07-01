77185d4091b3f94454f879a230ce8d8e2dca1a6f
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2023-07-01 22:55:39 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 4 ms, Memory: 3072 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+#define FAST_IO \
+    if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
+#define debug(x) \
+    if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugf(...) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "; printf(__VA_ARGS__); }
+#define debugc(c) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int n, k;
+int ans = INF;
+queue<pii> q;
+int visited[100001];
+
+bool in_range(int pos) {
+    return 0 <= pos && pos <= 100000;
+}
+
+int main(){
+    FAST_IO;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cin >> n >> k;
+    q.push({n, 0});
+
+    while (!q.empty()) {
+        pii item = q.front();
+        int pos = item.first, cost = item.second;
+        q.pop();
+
+        if (!in_range(pos)) continue;
+        if (visited[pos]) continue;
+        visited[pos] = true;
+        
+        if (pos == k) {
+            ans = min(ans, cost);
+            continue;
+        }
+        
+        q.push({pos * 2, cost});
+        q.push({pos - 1, cost + 1});
+        q.push({pos + 1, cost + 1});
+    }
+
+    cout << ans << endl;
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
