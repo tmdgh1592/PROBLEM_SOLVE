4caa4279a5e64dc2945c40cedbbe9ad653b5f69e
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2023-07-03 19:55:03 +0900

    [Silver III] Title: 1로 만들기, Time: 36 ms, Memory: 37052 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,40 +1,62 @@
 #include <bits/stdc++.h>
-#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define endl "\n"
 #define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
 #define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
 #define pii pair<int, int>
 #define all(v) (v).begin(), (v).end()
 #define pb push_back
-#define INF 987654321
-
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
 using namespace std;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+#define FAST_IO \
+    if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
+#define debug(x) \
+    if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugf(...) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "; printf(__VA_ARGS__); }
+#define debugc(c) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
 typedef long long ll;
 typedef unsigned long long ull;
 
 int n;
-vector<int> cache;
+int dp[1000001];
 
-int f(int now) {
-    if (now == 1) return 0;
-    if (cache[now] != -1) return cache[now];
+int f(int x) {
+    if (x == 1) return 0;
+    if (dp[x] != -1) return dp[x];
 
-    cache[now] = f(now - 1) + 1;
-    if (now % 3 == 0) cache[now] = min(cache[now], f(now / 3) + 1);
-    if (now % 2 == 0)cache[now] = min(cache[now], f(now / 2) + 1);
-
-    return cache[now];
+    int& ret = dp[x];
+    ret = 1 + f(x - 1);
+    if (x % 3 == 0)ret = min(ret, 1 + f(x / 3));
+    if (x % 2 == 0) ret = min(ret, 1 + f(x / 2));
+    
+    return ret;
 }
 
 int main(){
-    FAST;
+    FAST_IO;
 #ifndef ONLINE_JUDGE
+    clock_t start = clock();
     freopen("input.txt", "r", stdin);
 #endif
 
     cin >> n;
-    cache = vector<int>(n + 1, -1);
-    cout << f(n);
+    memset(dp, -1, sizeof(dp));
+    cout << f(n) << endl;
 
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
     return 0;
 }
