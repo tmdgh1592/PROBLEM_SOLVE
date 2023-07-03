1d3a5096753386088b8d5cc85cd963987dc5283c
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2023-07-03 20:59:39 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+#define FAST_IO \
+    if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
+#define debug(x) \
+    if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugf(...) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "; printf(__VA_ARGS__); }
+#define debugc(c) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int n;
+int dp[1001];
+
+int f(int x) {
+    if (x >= n) return 1;
+    if (dp[x] != -1) return dp[x] % 10007;
+
+    int& ret = dp[x];
+    ret = 0;
+
+    ret += f(x + 1);
+    if (x < n - 1) ret += f(x + 2);
+
+    return ret % 10007;
+}
+
+int main(){
+    FAST_IO;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cin >> n;
+    memset(dp, -1, sizeof(dp));
+    cout << f(0) << endl;
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
