5d1aed1bfc22640c13311cb4c9a109d5bf46a554
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2023-07-14 19:34:56 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2524 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+#define FAST_IO \
+    if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
+#define debug(x) \
+    if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugf(...) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "; printf(__VA_ARGS__); }
+#define debugc(c) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int n;
+int arr[10001];
+int dp[10001][3]; // i번째 까지 마셨을 때 마신 최대의 양
+
+int f(int x, int cnt) {
+    if (x == n) return 0;
+    if (dp[x][cnt] != -1) return dp[x][cnt];
+
+    int& ret = dp[x][cnt];
+    if (cnt < 2) {
+        ret = max(ret, arr[x] + f(x + 1, cnt + 1));
+        ret = max(ret, f(x + 1, 0));
+    } else {
+        ret = max(ret, f(x + 1, 0));
+    }
+    return ret;
+}
+
+int main(){
+    FAST_IO;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cin >> n;
+    rep(i, 0, n) cin >> arr[i];
+    memset(dp, -1, sizeof(dp));
+    cout << f(0, 0) << endl;
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
