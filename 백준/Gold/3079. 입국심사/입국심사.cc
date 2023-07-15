cb8c6625c2f3b134d3cf6d10e7e1618b32959d51
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2023-07-16 00:21:25 +0900

    [Gold V] Title: 입국심사, Time: 72 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Gold/3079. 입국심사/입국심사.cc
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+#define FAST_IO \
+    if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
+#define debug(x) \
+    if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugf(...) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "; printf(__VA_ARGS__); }
+#define debugc(c) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+ull n, m;
+ull arr[100000];
+
+bool check(ull total_time) {
+    ull cnt = 0;
+    rep(i, 0, n) cnt += total_time / arr[i];
+    return cnt >= m;
+}
+
+int main(){
+    FAST_IO;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cin >> n >> m;
+    rep(i, 0, n) cin >> arr[i];
+    sort(arr, arr + n);
+
+    ull lo = arr[0] - 1;
+    ull hi = arr[n - 1] * m + 1;
+
+    while(lo + 1 < hi) {
+        ull mid = (lo + hi) >> 1;
+
+        if (check(mid)) hi = mid;
+        else lo = mid;
+    }
+
+    cout << hi << endl;
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
