d35d6ae12c68a105ec121b12acc7de9880eba89d
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2023-07-21 00:55:22 +0900

    [Silver I] Title: 회의실 배정, Time: 28 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+#define FAST_IO \
+    if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
+#define debug(x) \
+    if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugf(...) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "; printf(__VA_ARGS__); }
+#define debugc(c) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+bool cmp(pii a, pii b) {
+    if (a.second == b.second) {
+        return a.first < b.first;
+    } else {
+        return a.second < b.second;
+    }
+}
+
+int n, a, b, res = 1;
+vector<pii> vec;
+
+int main(){
+    FAST_IO;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cin >> n;
+    rep(i, 0, n) {
+        cin >> a >> b;
+        vec.pb({a, b});
+    }
+    sort(all(vec), cmp);
+
+    int last_end = vec.front().second;
+    rep(i, 1, vec.sz) {
+        auto [start, end] = vec[i];
+        if (start < last_end) continue;
+        res++;
+        last_end = end;
+    }
+
+    cout << res << endl;
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
