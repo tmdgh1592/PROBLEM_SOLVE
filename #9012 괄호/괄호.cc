8b6a59537c4b63cc1a95dc5f6f30ed58a0de85ea
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2023-09-01 00:11:16 +0900

    [Silver IV 9012] Title: 괄호, Time: 0 ms, Memory: 2024 KB -NKLCBHub

---
+++ #9012 괄호/괄호.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int n;
+char x;
+vector<char> vec;
+
+int main(){
+    FAST_IO;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cin >> n;
+    rep(i, 0, n) {
+        vec.clear();
+        string line;
+        cin >> line;
+        bool flag = 0;
+
+        for(char c : line) {
+            if (c == '(') vec.pb(c);
+            else if (c == ')') {
+                if (!vec.empty() && vec.back() == '(') {
+                    vec.pop_back();
+                }
+                else {
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        if (!vec.empty() || flag) cout << "NO" << endl;
+        else cout << "YES" << endl;
+    }
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
