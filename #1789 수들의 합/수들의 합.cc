09a3434a26a3ccf1c0b1fcf40b7ec5e72cb1918a
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2023-09-07 00:22:40 +0900

    [Silver V 1789] Title: 수들의 합, Time: 0 ms, Memory: 2020 KB -NKLCBHub

---
+++ #1789 수들의 합/수들의 합.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+ll s, sum, cur, cnt;
+
+int main(){
+    FAST_IO;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cin >> s;
+
+    cur = 1;
+    cnt = 0;
+    sum = 0;
+
+    while(1) {
+        sum += cur;
+        cnt++;
+        cur++;
+
+        if (sum > s) {
+            cnt--;
+            break;
+        }
+    }
+
+    cout << cnt;
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
