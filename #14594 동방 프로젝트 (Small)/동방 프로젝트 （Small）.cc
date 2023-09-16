1c45b57c0cff8f98f840bc85b7a735a54ca9df07
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2023-09-17 00:49:28 +0900

    [Silver IV 14594] Title: 동방 프로젝트 (Small), Time: 0 ms, Memory: 2020 KB -NKLCBHub

---
+++ #14594 동방 프로젝트 (Small)/동방 프로젝트 （Small）.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int n, t, x, y;
+
+int main(){
+    FAST_IO;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cin >> n >> t;
+    bool room[n];
+    memset(room, true, sizeof(room));
+
+    rep(i, 0, t) {
+        cin >> x >> y;
+        rep(j, x - 1, y - 1) {
+            room[j] = 0;
+        }
+    }
+    int res = 0;
+    REP(i, 0, n - 1) res += room[i];
+    cout << res;
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
