768e916936298fa04b5128727a1e82d81bef4929
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2023-09-18 23:41:42 +0900

    [Bronze II 10870] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -NKLCBHub

---
+++ #10870 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define sz size()
+#define PIV (1 << 20)
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int n;
+
+int main(){
+    FAST_IO;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cin >> n;
+    ll left = 0, right = 1;
+    if (n == 0) {
+        cout << 0; 
+        return 0;
+    }
+    if (n == 1) {
+        cout << 1; 
+        return 0;
+    }
+    ll res = 0;
+
+    rep(i, 0, n - 1) {
+        ll tmp = right;
+        res = left + right;
+        left = tmp;
+        right = res;
+    }
+
+    cout << res;
+
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
