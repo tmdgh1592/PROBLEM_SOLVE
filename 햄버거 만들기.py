659288af27c552e3eee922cd867b02860ece6799
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2024-03-03 17:25:38 +0900

    feat: 햄버거 만들기 문제풀이

---
+++ 햄버거 만들기.py
@@ -0,0 +1,9 @@
+def solution(ingredient):
+    answer = 0
+    s = []
+    for i in ingredient:
+        s.append(i)
+        if s[-4:] == [1,2,3,1]:
+            answer += 1
+            del s[-4:]
+    return answer
