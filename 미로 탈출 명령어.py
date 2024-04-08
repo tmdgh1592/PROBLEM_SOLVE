1c8796b40e80ec9905be66aa51209d5046e98ff7
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2024-04-08 14:01:53 +0900

    feat: 미로 탈출 명령어 풀이정정

--- 미로 탈출 명령어.py
+++ 미로 탈출 명령어.py
@@ -1,24 +1,26 @@
-import math
+from collections import deque
 
+dx, dy = [1, 0, 0, -1], [0, -1, 1, 0]
+dirs = {0:'d', 1:'l', 2:'r', 3:'u'}
 
-def check(bin_num):
-    if len(bin_num) == 1: return True
+def in_range(n, m, x, y):
+    return 0 <= x < n and 0 <= y < m
 
-    root = len(bin_num) // 2
-    if bin_num[root] == '0':
-        return '1' not in bin_num
-
-    return check(bin_num[:root]) and check(bin_num[root + 1:])
-
-
-def solution(numbers):
-    answer = []
-
-    for num in numbers:
-        num = bin(num)[2:]
-        digit = 2 ** (int(math.log(len(num), 2)) + 1) - 1
-        num = "0" * (digit - len(num)) + num
-
-        answer.append(1 if check(num) else 0)
-
-    return answer
+def solution(n, m, sx, sy, ex, ey, k):
+    sx, sy, ex, ey = sx-1, sy-1, ex-1, ey-1
+    q = deque([(sx, sy, '')])
+    
+    while q:
+        x, y, command = q.popleft()
+        
+        if len(command) == k and x == ex and y == ey: return command
+        
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            path = command + dirs[i]
+            if not in_range(n, m, nx, ny): continue
+            if k - len(path) < abs(nx - ex) + abs(ny - ey): continue
+            q.append((nx, ny, path))
+            break
+    
+    return "impossible"
